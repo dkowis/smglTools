0247e5a97a2fa58f062b19a15a237697e3e9c288
David Kowis <dkowis@shlrm.org>
2010-09-08 22:51:14 -0500

    Getting some things done...?
    Have to figure out how to actually test things...

--- mirrorMaker/features/step_definitions/mirrorMaker_steps.rb
+++ mirrorMaker/features/step_definitions/mirrorMaker_steps.rb
@@ -1,14 +1,18 @@
 Given /^a grimoire output in yaml$/ do
 	# assume the grimoire output exists already and that's okay.
 	# TODO: figure out how to ensure the yaml output exists, probably via a fixture or somethign simulated
+	@test_path = File.expand_path("../../../test", __FILE__)
+	@grimoire_yaml = File.new(File.join(@test_path,"/sample.yaml"))
+
 end
 
 Given /^all files are already in the spool directory$/ do
+	#TODO: how do I put the files in here already? I should be using mocks, yes?
   pending # express the regexp above with the code you wish you had
 end
 
 When /^the downloader is run$/ do
-  pending # express the regexp above with the code you wish you had
+	MirrorMaker::Downloader.new.start @grimoire_yaml
 end
 
 Then /^nothing should be downloaded$/ do
