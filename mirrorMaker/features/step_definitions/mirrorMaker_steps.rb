18ac7c878e1a1f09f7d63999cd70b1921df4ffd5
David Kowis <dkowis@shlrm.org>
2010-09-08 22:42:52 -0500

    Started creating features.
    But now it's time to sleep :(

---
+++ mirrorMaker/features/step_definitions/mirrorMaker_steps.rb
@@ -0,0 +1,37 @@
+Given /^a grimoire output in yaml$/ do
+	# assume the grimoire output exists already and that's okay.
+	# TODO: figure out how to ensure the yaml output exists, probably via a fixture or somethign simulated
+end
+
+Given /^all files are already in the spool directory$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the downloader is run$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^nothing should be downloaded$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^some files are invalid$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the bad files will be downloaded$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the files will be verified$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^no files are in the spool directory$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the missing files will be downloaded$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
