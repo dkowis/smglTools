1146993a7e36b7ab12eb6cf05c6317e33425db57
David Kowis <dkowis@shlrm.org>
2012-01-15 00:10:09 -0600

    I don't remember what I was doing with this, but it's pushed

--- mirrorMaker/spec/mirrorhelper/sourcemage_mirror_spec.rb
+++ mirrorMaker/spec/mirrorhelper/sourcemage_mirror_spec.rb
@@ -5,7 +5,21 @@ describe SourceMage::Mirror do
   it "summons all the spells from each grimoire" do
     mirror = SourceMage::Mirror.new
 
-    pending "not done yet! Mock up all the things and describe the execution"
+    interface = double(SourceMage::Interface)
+
+    SourceMage::Interface.should_receive(:new).and_return(interface)
+
+    data = {
+        sample: %w{sample1 sample2 sample3},
+        sample2: %w{example1 example2 example3}
+    }
+
+    puts data.keys
+    interface.should_receive(:list_grimoires).and_return(data.keys)
+    interface.should_receive(:list_spells).with("sample").and_return(%w{sample1 sample2 sample3})
+    interface.should_receive(:list_spells).with("sample2").and_return(%w{example1 example2 example3})
+
+
 
     expect { mirror.mirror }.
         not_to raise_error
