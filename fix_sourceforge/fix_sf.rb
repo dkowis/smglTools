e33d561dd5c9abefb3efda4d77a10b6c0dca17cc
David Kowis <dkowis@shlrm.org>
2013-02-16 22:34:04 -0600

    Adding a script to do stuff with sf urls

---
+++ fix_sourceforge/fix_sf.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'open3'
+
+input_file = ARGV[0]
+
+
+File.readlines(input_file).each do |spell|
+    urls = `gaze source_urls #{spell}`.split("\n")
+    urls.each do |url|
+        if url.include? "sourceforge"
+            stdout, stderr, status = Open3("wget --tries=1 #{url} -O /dev/null")
+            if status.success?
+                puts "SUCCESS #{spell} - #{url}"
+            else
+                puts "   FAIL #{spell} - #{url}"
+            end
+        end
+    end
+
+end
