b5a693d76beb0984b0624ca20a194ea9f3c75dfb
David Kowis <dkowis@shlrm.org>
2014-11-09 19:59:42 -0600

    Output source hashes as well, why not

--- gDownloader/src/main/resources/constant.sh
+++ gDownloader/src/main/resources/constant.sh
@@ -21,6 +21,36 @@ function my_get_spell_source_urls() {
     done
 }
 
+function my_get_spell_source_hash() {
+    local sourcevar=SOURCE${1}
+    local hashvar="${sourcevar}_HASH"
+    local ignorevar="${sourcevar}_IGNORE"
+
+    if [[ "${!hashvar}" == "" ]]; then
+        # no hash
+        if [[ "${!ignorevar}" == "signature" ]]; then
+            echo -n "${!ignorevar}"
+        else
+            local gpgvar="${sourcevar}_GPG"
+            if [[ "${!gpgvar}" == "" ]] ; then
+                # not a gpg either, must be MD5 or no hash!
+                local md5var="MD5[*]"
+                if [[ "${!md5var[$1]}" == "" ]]; then
+                    # no verification provided!
+                    echo -n "none"
+                else
+                    #going to break the rules here and add md5: to it
+                    echo -n "md5:${!md5var[$1]}"
+                fi
+            else
+                echo -n ${!gpgvar}
+            fi
+        fi
+	else
+		echo -n ${!hashvar}
+	fi
+}
+
 function spell_json() {
         # Set the current spell info, so I have magic information I can do stuff with!
         codex_set_current_spell $1
@@ -35,8 +65,11 @@ function spell_json() {
             source_var="SOURCE${source_num}"
             if [[ "${!source_var}" != "" ]]; then
                 # create a source entry and start an array for the urls
-                source_files="${source_files} \"${!source_var}\":["
+                source_files="${source_files} \"${!source_var}\":{"
+                # output the source hash for this source
+                source_files="${source_files} \"hash\": \"$(my_get_spell_source_hash ${source_num})\","
                 #In here, get each source url for this source
+                source_files="${source_files} \"urls\": ["
                 for source_url in $(my_get_spell_source_urls ${source_num}); do
                     # Add an entry in the array for each url
                     source_files="${source_files}\"${source_url}\","
@@ -44,7 +77,9 @@ function spell_json() {
                 # take off the dangling comma
                 source_files="${source_files%?}"
                 # close this array
-                source_files="${source_files}],"
+                source_files="${source_files}]"
+                # close the object
+                source_files="${source_files}},"
             fi
         done
         # take off the dangling source files comma
