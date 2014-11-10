ce334f84828e87b7cf2910338788aa51f9713315
David Kowis <dkowis@shlrm.org>
2014-11-09 20:42:18 -0600

    Adding all the akka... for now

--- gDownloader/project/dependencies.scala
+++ gDownloader/project/dependencies.scala
@@ -2,12 +2,19 @@ import sbt._
 import Keys._
 
 object Library {
+
+  val akkaVersion = "2.3.6"
+  val log4jVersion = "2.1"
+
   val scalatest = "org.scalatest" %% "scalatest" % "2.2.1"
   val scopt = "com.github.scopt" %% "scopt" % "3.2.0"
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0"
-  val log4j2_core = "org.apache.logging.log4j" % "log4j-core" % "2.1"
-  val log4j2_api = "org.apache.logging.log4j" % "log4j-api" % "2.1"
-  val log4j2_slf4j = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.1"
+  val log4j2_core = "org.apache.logging.log4j" % "log4j-core" % log4jVersion
+  val log4j2_api = "org.apache.logging.log4j" % "log4j-api" % log4jVersion
+  val log4j2_slf4j = "org.apache.logging.log4j" % "log4j-slf4j-impl" % log4jVersion
+  val akka = "com.typesafe.akka" %% "akka-actor" % akkaVersion
+  val akka_slf4j = "com.typesafe.akka" %% "akka-slf4j" % akkaVersion
+  val sprayJson = "io.spray" %% "spray-json" % "1.3.1"
 }
 
 object Dependencies {
@@ -16,6 +23,9 @@ object Dependencies {
 
   val deps = Seq(
     scalatest % "test",
+    akka,
+    akka_slf4j,
+    sprayJson,
     //scopt,
     log4j2_core,
     log4j2_api,
