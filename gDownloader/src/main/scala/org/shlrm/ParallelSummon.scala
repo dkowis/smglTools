b63ffef1b3e14894bc2fc36475a6fa710f27a1fe
David Kowis <dkowis@shlrm.org>
2014-11-11 19:28:57 -0600

    Trying to remove the memory leak from this guy :(

--- gDownloader/src/main/scala/org/shlrm/ParallelSummon.scala
+++ gDownloader/src/main/scala/org/shlrm/ParallelSummon.scala
@@ -1,17 +1,20 @@
 package org.shlrm
 
+import java.util.concurrent.Executors
+
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.annotation.tailrec
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{ExecutionContext, Await, Future}
 
 object ParallelSummon extends App with LazyLogging {
 
-  import scala.concurrent.ExecutionContext.Implicits.global
   import scala.sys.process._
 
   logger.info("Starting up!")
 
+  //Create a fixed thread pool executor for great justice
+  implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(4))
 
   case class SimpleSummoner(grimoire: String, section: String, spell: String) {
 
@@ -20,13 +23,17 @@ object ParallelSummon extends App with LazyLogging {
     val stdOut = new StringBuffer
     val stdErr = new StringBuffer
 
-    val outputLogger = ProcessLogger(
-      line => stdOut.append(line).append("\n"),
-      line => stdErr.append(line).append("\n"))
-
     val exitStatus = Future {
-      s"summon -g $grimoire $spell" !< outputLogger
+      //The processLogger does threads in the background I don't want them
+      //Lets try to use less values, and make sure we're not keeping a reference to this guy around
+      // I assume that the threads will be closed when the result is finished.... I hope?
+      val result = s"summon -g $grimoire $spell" !< ProcessLogger(
+        line => stdOut.append(line).append("\n"),
+        line => stdErr.append(line).append("\n")
+      )
+      result
     }
+
     override def toString(): String = {
       s"$grimoire/$section/$spell"
     }
@@ -35,7 +42,7 @@ object ParallelSummon extends App with LazyLogging {
   //NOTE: have to execute everything inside of some bash
   //Get all the spells
   // Always use a def for streams, not a val, you want to not keep it all around forever!
-  def spellStream = Seq("bash", "-c", ". /etc/sorcery/config; codex_get_all_spells").lineStream.map {l =>
+  def spellStream = Seq("bash", "-c", ". /etc/sorcery/config; codex_get_all_spells").lineStream.map { l =>
     logger.debug(s"Found spell $l")
 
     val triad = l.replaceAll("/var/lib/sorcery/codex/", "").split("/")
@@ -43,13 +50,14 @@ object ParallelSummon extends App with LazyLogging {
     SimpleSummoner(triad(0), triad(1), triad(2))
   }
 
-//  val iterator = spellStream.toIterator
-//
-//  iterator.foreach { item =>
-//
-//  }
+  //  val iterator = spellStream.toIterator
+  //
+  //  iterator.foreach { item =>
+  //
+  //  }
 
   logger.info("Created spellInfoStream")
+
   //Need to not convert this stream to a list... It keeps too many things around
 
   /**
@@ -58,8 +66,8 @@ object ParallelSummon extends App with LazyLogging {
    * @param stream
    */
   @tailrec
-  def consumeStream(stream: Stream[SimpleSummoner]):Unit = {
-    if(stream.nonEmpty) {
+  def consumeStream(stream: Stream[SimpleSummoner]): Unit = {
+    if (stream.nonEmpty) {
       //If the stream is non empty, take one, and do something with it, then recurse
       val summon = stream.head
       import scala.concurrent.duration._
@@ -75,7 +83,7 @@ object ParallelSummon extends App with LazyLogging {
             }
           }
         } catch {
-          case e:Exception =>
+          case e: Exception =>
             logger.error(s"An exception was caught while awaiting a result for $summon", e)
         }
       }
